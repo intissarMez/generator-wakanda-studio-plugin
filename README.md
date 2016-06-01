13aa834705c72a6836601cb428c2f9cee627de25
intissarMez <intissar.elmezrouai@gmail.com>
2016-06-01 13:35:26 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# generator-wakanda-studio-plugin
+> Generate a basic structure to get up and running with wakanda plugin  developemet
+
+## Test
+
+```bash
+npm install
+npm link
+```
+
+Navigate to a folder where you want to scaffold your plugin then run:
+
+```bash
+yo wakanda-studio-plugin
+```
---
+++ generators/app/index.js
@@ -0,0 +1,68 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.Base.extend({
+
+  prompting: function () {
+    this.log(yosay(
+      'Welcome to the Wakanda Studio Plugin Generator!'
+    ));
+
+   return this.prompt([{
+     type    : 'input',
+     name    : 'name',
+     message : 'Your plugin  name : ',
+     default : this.appname
+   }, {
+     type    : 'input',
+     name    : 'description',
+     message : 'What does your plugin do ?'
+   }]).then(function (answers) {
+     this.props = answers
+     this.log(answers.name);
+   }.bind(this));
+ },
+
+  writing: function () {
+    this.fs.copyTpl(
+      this.templatePath('manifest.json'),
+      this.destinationPath('manifest.json'),{
+         name: this.props.name,
+         description : this.props.description
+      }
+    );
+
+    this.fs.copy(
+      this.templatePath('index.js'),
+      this.destinationPath('index.js')
+    );
+
+    this.fs.copy(
+      this.templatePath('YOUR_EXTENSION_ICON.png'),
+      this.destinationPath('YOUR_EXTENSION_ICON.png')
+    );
+    this.fs.copy(
+      this.templatePath('quickstart.txt'),
+      this.destinationPath('quickstart.txt')
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('README.md'),
+      this.destinationPath('README.md'),{
+         name: this.props.name,
+         description : this.props.description
+      }
+    );
+
+    this.fs.copy(
+      this.templatePath('test/test.js'),
+      this.destinationPath('test/test.js')
+    );
+  },
+
+  install: function () {
+    this.installDependencies();
+  }
+});
