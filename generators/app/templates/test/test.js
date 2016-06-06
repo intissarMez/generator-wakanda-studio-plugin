3b5433de997cbbd644c9347dcf94cebebb4d2f0f
intissarMez <intissar.elmezrouai@gmail.com>
2016-06-06 17:23:30 +0000

    Add test.js skeleton based on mocha and chai

---
+++ generators/app/templates/test/test.js
@@ -0,0 +1,11 @@
+/*
+  This test is based on mocha and chai, please refer to their official doc for more details  https://mochajs.org/  http://chaijs.com/
+*/
+
+const expect = require('chai').expect;
+
+describe('Basic Wakanda Plugin Test', function () {
+	it('should pass when everything is okay', function () {
+		expect(true).to.be.true;
+	});
+});
