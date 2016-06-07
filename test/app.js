d5161b224b373fa17c865b5cf3da4ceffa46eaae
intissarMez <intissar.elmezrouai@gmail.com>
2016-06-07 11:11:09 +0000

    Add Generator test

--- test/app.js
+++ test/app.js
@@ -2,17 +2,51 @@
 var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
+//var fs = require('fs');
 
-describe('generator-wakanda-studio-plugin:app', function () {
+describe('generator-wakanda-studio-plugin :', function () {
   before(function () {
     return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
+      .withPrompts({name: "some name",description: "some description",version: "0.1.0",author: "some author",location: "some location"})
       .toPromise();
   });
 
-  it('creates files', function () {
+  it('creates the top level file,package.json,quickstart.md and README.md', function () {
     assert.file([
-      'dummyfile.txt'
+      'index.js','package.json','quickstart.md','README.md'
     ]);
   });
+
+/*
+  var expectedResults = {
+  "name": "generator-wakanda-studio-plugin",
+  "version": "0.1.0",
+  "description": "Generate a basic structure to get up and running with wakanda plugin developemet",
+  "repository": "https://github.com/wakanda/generator-wakanda-studio-plugin",
+  "homepage": "https://wakanda.github.io",
+  "license": "MIT",
+  "author" :"Wakanda SAS",
+  "files": [
+    "generators"
+  ],
+  "main": "app/index.js",
+  "keywords": [
+    "yeoman-generator",
+    "wakanda-studio-plugin-generator",
+    "wakanda studio"
+  ],
+  "dependencies": {
+    "yeoman-generator": "^0.23.0",
+    "chalk": "^1.0.0",
+    "yosay": "^1.0.0"
+    },
+    "devDependencies": {
+      "mocha": "*"
+    }
+  }
+  var jsonDATA = fs.readFileSync('package.json', 'utf8');
+  var actualResults = JSON.parse(jsonDATA);
+  assert.deepEqual(expectedResults, actualResults);
+*/
+
 });
